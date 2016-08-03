863f5334ec977eaa55283136d81a040bbc0cb564
khou22 <kevin.ch.hou@gmail.com>
2016-08-03 15:54:03 -0700

    Added more tabs to tab view controller

---
+++ SampleTabProject/SampleTabProject/LaunchScreen.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  SampleTabProject
+//
+//  Created by Breathometer on 8/3/16.
+//  Copyright © 2016 KevinHou. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
