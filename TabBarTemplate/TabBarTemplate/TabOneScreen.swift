7ac49a8f9ba1f8181253621083ecf1271ecc921b
khou22 <kevin.ch.hou@gmail.com>
2016-08-09 10:08:59 -0700

    Created template project: simple tab bar

---
+++ TabBarTemplate/TabBarTemplate/TabOneScreen.swift
@@ -0,0 +1,25 @@
+//
+//  TabOneScreen.swift
+//  TabBarTemplate
+//
+//  Created by Breathometer on 8/9/16.
+//  Copyright © 2016 KevinHou. All rights reserved.
+//
+
+import UIKit
+
+class TabOneScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabBarTemplate/TabBarTemplate/TabTwoScreen.swift
@@ -0,0 +1,25 @@
+//
+//  TabTwoScreen.swift
+//  TabBarTemplate
+//
+//  Created by Breathometer on 8/9/16.
+//  Copyright © 2016 KevinHou. All rights reserved.
+//
+
+import UIKit
+
+class TabTwoScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
